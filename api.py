555d8c83cc0459f6e31fad9a180d1d5b51c63d29
João Victor <joaovictorinter55@gmail.com>
2023-03-31 22:38:26 -0300

    Rest API process request script upload
    This is the main file of the the API, this script process de request and call the function

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+@app.route('/api/<classe>/<modulo>', methods=['GET'])
+def rest(classe, modulo):    
+    root = __import__(classe)
+    
+    function = getattr(root, modulo)
+
+    response = function()
+
+    return response        
+
+if __name__ == '__main__':                
+    print("\n" * 500) 
+    print("SERVER HAS BEEN STARTED - PYcharm is ON!")
+    print("ENDPOINT: http://127.0.0.1:5000/api/<classe>/<modulo>")
+    print("\n" * 2)
+    print("DESENVOLVIDO POR JOÃO VICTOR ")
+    print("\n" * 2)
+    print(";) Viver é o suícidio menos doloroso ;)")
+    print("\n" * 10)
+    app.run()    
+else:
+    print("\n" * 500)
+    print("NÃO FOI POSSÍVEL INICIALIZAR O SERVIDOR")
+    print("\n" * 10)
